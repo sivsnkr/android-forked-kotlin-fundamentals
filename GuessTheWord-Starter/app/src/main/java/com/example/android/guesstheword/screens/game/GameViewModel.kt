e58309c7235e56d6bd1df1e351c4b5331e0a549f
sivsnkr <ced17i026@iiitdm.ac.in>
2021-11-20 13:02:11 +0530

    display timer on the screen

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
@@ -21,14 +23,17 @@ class GameViewModel : ViewModel() {
     val gameFinishedEvent : LiveData<Boolean>
         get() = _gameFinishedEvent
 
-    private val _currentTime = MutableLiveData<Long>()
-    val currentTime : LiveData<Long>
-        get() = _currentTime
+    private val currentTime = MutableLiveData<Long>()
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList : MutableList<String>
 
     private val timer : CountDownTimer
+
+    // The String version of the current time
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
 
     companion object{
         // Time when the game is over
@@ -50,11 +55,11 @@ class GameViewModel : ViewModel() {
         nextWord()
         timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
             override fun onTick(millisUntilFinished: Long) {
-                _currentTime.value = millisUntilFinished/ONE_SECOND
+                currentTime.value = millisUntilFinished/ONE_SECOND
             }
 
             override fun onFinish() {
-                _currentTime.value = DONE
+                currentTime.value = DONE
                 onGameFinish()
             }
         }
