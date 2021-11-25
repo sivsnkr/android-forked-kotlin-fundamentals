710f6580b8581e63aa9d2e30f26423725bc12f28
sivsnkr <ced17i026@iiitdm.ac.in>
2021-11-25 15:19:49 +0530

    add binding Adapters to RecycleView

--- RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ RecyclerViewFundamentals-Starter/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -45,27 +45,9 @@ class ViewHolder private constructor(private val binding: ListItemSleepNightBind
     }
 
     fun bind(item: SleepNight) {
-        val res = itemView.context.resources
         binding.apply {
-            sleepLength.text = convertDurationToFormatted(
-                item.startTimeMilli, item.endTimeMilli, res
-            )
-
-            qualityString.text = convertNumericQualityToString(
-                item.sleepQuality, res
-            )
-
-            qualityImage.setImageResource(
-                when (item.sleepQuality) {
-                    0 -> R.drawable.ic_sleep_0
-                    1 -> R.drawable.ic_sleep_1
-                    2 -> R.drawable.ic_sleep_2
-                    3 -> R.drawable.ic_sleep_3
-                    4 -> R.drawable.ic_sleep_4
-                    5 -> R.drawable.ic_sleep_5
-                    else -> R.drawable.ic_sleep_active
-                }
-            )
+            sleep = item
+            executePendingBindings()
         }
     }
 }
